0a71ae7851456be3a0f378ca68993a7776acba7d
devi-bis <dbiswabh@edu.uwaterloo.ca>
2017-07-08 16:53:08 -0400

    Update ServoCRtest2.c

--- ServoCRtest2.c
+++ ServoCRtest2.c
@@ -1,3 +1,5 @@
+//Test program for Tetrix continuous servo
+
 #include "NXTServo-lib-UW.c"
 
 
@@ -8,18 +10,18 @@ task main()
 	//NOTE: Motor directions are determined by looking down the shaft, TOWARDS the motor
 
 
-	while(1){														//Continuous loop
+	while(1){				//Continuous loop
 
 	setServoSpeed(S1, 1, -25, 13, 39);	//Run the motor @ speed 25 past the deadband, Clockwise
-	while (nNxtButtonPressed != 3){}		//Press & release orange button to switch speed
+	while (nNxtButtonPressed != 3){}	//Press & release orange button to switch speed
 	while (nNxtButtonPressed != -1){}
 
-	setServoSpeed(S1, 1, 25, 13, 39);		//Run the motor @ speed 25 past the deadband, Counter-Clockwise
-	while (nNxtButtonPressed != 3){}		//Press & release orange button to switch speed
+	setServoSpeed(S1, 1, 25, 13, 39);	//Run the motor @ speed 25 past the deadband, Counter-Clockwise
+	while (nNxtButtonPressed != 3){}	//Press & release orange button to switch speed
 	while (nNxtButtonPressed != -1){}
 
-	setServoSpeed(S1, 1, 25);						//Stop motor (uses unconfigured setspeed func. to avoid creeping on the deadband boundary, manually sets speed in the middle of deadband)
-	while (nNxtButtonPressed != 3){}		//Press & release orange button to switch speed
+	setServoSpeed(S1, 1, 25);		//Stop motor (uses unconfigured setspeed func. to avoid creeping on the deadband boundary, manually sets speed in the middle of deadband)
+	while (nNxtButtonPressed != 3){}	//Press & release orange button to switch speed
 	while (nNxtButtonPressed != -1){}
 
 	}
