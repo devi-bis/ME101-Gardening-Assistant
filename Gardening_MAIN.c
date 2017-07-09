3a7c7a81824c3c021ead22f6e535f8cc582d877f
devi-bis <dbiswabh@edu.uwaterloo.ca>
2017-07-08 21:55:20 -0400

    Add files via upload

---
+++ Gardening_MAIN.c
@@ -0,0 +1,37 @@
+#include "NXTServo-lib-UW.c"
+
+void CRServoCW(int CRCWdur)							//Rotate Continuos Servo Clockwise for specified duration in ms
+{
+	setServoSpeed(S1, 1, 25, 13, 39);			//Configured to account for servo deadband (13 - 39)
+	wait1Msec(CRCWdur);										//Turn CW for user specified duration
+	setServoSpeed(S1, 1, 25);							//Unconfigured setServoSpeed function to avoid creep at deadband boundary, manually set speed to center of deadband to ensure 0 creep
+}
+
+void CRServoCCW(int CRCCWdur)						//Rotate Continuos Servo Counter-Clockwise for specified duration in ms
+{
+	setServoSpeed(S1, 1, -25, 13, 39);		//Configured to account for servo deadband (13 - 39)
+	wait1Msec(CRCCWdur);									//Turn CCW for user specified duration
+	setServoSpeed(S1, 1, 25);							//Unconfigured setServoSpeed function to avoid creep at deadband boundary, manually set speed to center of deadband to ensure 0 creep
+}
+
+void DrillDown(int Dist, bool Dir)			//Moves Drill by user specified distance (in mm), in user specified direction (true for down, false for up)
+{
+	nMotorEncoder[motorB] = 0;
+	if (Dir){
+		while (nMotorEncoder[motorB] < (Dist/(PI*23))*360){motor[motorB] = 80;}
+	}
+	else {
+		while (nMotorEncoder[motorB] > -(Dist/(PI*23))*360){motor[motorB] = -80;}
+	}
+	motor[motorB] = 0;
+}
+
+task main()
+{
+	SensorType[S1] = sensorI2CCustom9V;				//Declare Tetrix Servo Controller Port
+	SensorType[S2] = sensorColorNxtFULL;			//Declare	Sensor Ports
+//	SensorType[S3] = INTENSITY SENSOR
+
+
+
+}
